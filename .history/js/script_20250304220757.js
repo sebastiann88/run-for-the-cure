e591d865f680e6460f2bdc4b92612a52f760a21e
sebastiann88 <sebastian.nadeau45@gmail.com>
2025-03-17 18:36:37 -0400

    feat(all): create character animation

---
+++ .history/js/script_20250304220757.js
@@ -0,0 +1,60 @@
+$('html').imagesLoaded( function() {
+  // images have loaded
+
+const html = document.documentElement;
+const canvas = document.getElementById("image-sequence");
+const context = canvas.getContext("2d");
+
+const frameCount = 140;
+const currentFrame = index => (
+  `./frames/` + '0' + `${index.toString().padStart(4, '0')}.webp`
+)
+
+const preloadImages = () => {
+  for (let i = 1; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
+  }
+};
+
+preloadImages();
+
+const img = new Image()
+img.src = currentFrame(1);
+canvas.width=1920;
+canvas.height=1820;
+img.onload=function(){
+  context.drawImage(img, 0, 0);
+}
+
+const updateImage = index => {
+  img.src = currentFrame(index);
+  context.drawImage(img, 0, 0);
+}
+
+window.addEventListener('scroll', () => {  
+  const scrollTop = html.scrollTop;
+  const maxScrollTop = html.scrollHeight - window.innerHeight;
+  const scrollFraction = scrollTop / maxScrollTop;
+  const frameIndex = Math.min(
+    frameCount - 1,
+    Math.ceil(scrollFraction * frameCount)
+  );
+  
+  requestAnimationFrame(() => updateImage(frameIndex + 1))
+});
+
+const mySong = document.getElementById("mySong");
+const icon = document.getElementById("icon");
+
+icon.onclick = function() {
+  if(mySong.paused) {
+    mySong.play();
+    icon.src = "./media/pause.png"
+  } else {
+    mySong.pause();
+    icon.src = "./media/play.png"
+  }
+}
+
+});
---
+++ .history/js/script_20250317103721.js
@@ -0,0 +1,74 @@
+$('html').imagesLoaded( function() {
+  // images have loaded
+
+const html = document.documentElement;
+const canvas = document.getElementById("image-sequence");
+const context = canvas.getContext("2d");
+
+const frameCount = 140;
+const currentFrame = index => (
+  `./frames/` + '0' + `${index.toString().padStart(4, '0')}.webp`
+)
+
+const preloadImages = () => {
+  for (let i = 1; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
+  }
+};
+
+preloadImages();
+
+const img = new Image()
+img.src = currentFrame(1);
+canvas.width=1920;
+canvas.height=1820;
+img.onload=function(){
+  context.drawImage(img, 0, 0);
+}
+
+const updateImage = index => {
+  img.src = currentFrame(index);
+  context.drawImage(img, 0, 0);
+}
+
+window.addEventListener('scroll', () => {  
+  const scrollTop = html.scrollTop;
+  const maxScrollTop = html.scrollHeight - window.innerHeight;
+  const scrollFraction = scrollTop / maxScrollTop;
+  const frameIndex = Math.min(
+    frameCount - 1,
+    Math.ceil(scrollFraction * frameCount)
+  );
+  
+  requestAnimationFrame(() => updateImage(frameIndex + 1))
+});
+
+const mySong = document.getElementById("mySong");
+const icon = document.getElementById("icon");
+
+icon.onclick = function() {
+  if(mySong.paused) {
+    mySong.play();
+    icon.src = "./media/pause.png"
+  } else {
+    mySong.pause();
+    icon.src = "./media/play.png"
+  }
+}
+
+let ignoreScrollEvents = false
+  function syncScroll(element1, element2) {
+    element1.scroll(function (e) {
+      var ignore = ignoreScrollEvents
+      ignoreScrollEvents = false
+      if (ignore) return
+
+      ignoreScrollEvents = true
+      element2.scrollTop(element1.scrollTop())
+    })
+  }
+  syncScroll($("#div1"), $("#div2"))
+  syncScroll($("#div2"), $("#div1"))
+
+});
