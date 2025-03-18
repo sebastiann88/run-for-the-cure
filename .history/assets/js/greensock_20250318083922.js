57a17a0fd5c0e93310e236909c05b631a23ef883
sebastiann88 <sebastian.nadeau45@gmail.com>
2025-03-18 09:32:59 -0400

    style(all): style landing page

---
+++ .history/assets/js/greensock_20250318083922.js
@@ -0,0 +1,67 @@
+// Portfolio
+
+gsap.to('.section-1', {
+	scrollTrigger: {
+		trigger: ".section-1",
+		toggleActions: "play none none reverse",
+		toggleClass: {targets: ".section-1", className: ".portfolio-hide"}
+
+	},
+ 	duration: 3.0,
+ 	opacity: 1
+})
+
+gsap.to('.section-2', {
+	scrollTrigger: {
+		trigger: ".section-2",
+		toggleActions: "play none none reverse",
+		toggleClass: {targets: ".section-2", className: ".portfolio-hide"}
+
+	},
+ 	duration: 3.0,
+ 	opacity: 1
+})
+
+gsap.to('.section-3', {
+	scrollTrigger: {
+		trigger: ".section-3",
+		toggleActions: "play none none reverse",
+		toggleClass: {targets: ".section-3", className: ".portfolio-hide"}
+
+	},
+ 	duration: 3.0,
+ 	opacity: 1
+})
+
+gsap.to('.section-4', {
+	scrollTrigger: {
+		trigger: ".section-4",
+		toggleActions: "play none none reverse",
+		toggleClass: {targets: ".section-4", className: ".portfolio-hide"}
+
+	},
+ 	duration: 3.0,
+ 	opacity: 1
+})
+
+gsap.to('.section-5', {
+	scrollTrigger: {
+		trigger: ".section-5",
+		toggleActions: "play none none reverse",
+		toggleClass: {targets: ".section-5", className: ".portfolio-hide"}
+
+	},
+ 	duration: 3.0,
+ 	opacity: 1
+})
+
+gsap.to('.reveal', {
+	scrollTrigger: {
+		trigger: ".reveal",
+		toggleActions: "play none none reverse",
+		toggleClass: {targets: ".reveal", className: ".portfolio-hide"}
+
+	},
+ 	duration: 0.5,
+ 	opacity: 1
+})
